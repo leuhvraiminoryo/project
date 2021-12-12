506f33d54bb1f3ed1398fe3e7d11e40f96f24ad9
leuhvraiminoryo <50586611+leuhvraiminoryo@users.noreply.github.com>
2021-12-12 11:18:19 +0100

    placing buildings : part 1
    WIP par rapport au placement d'un nouveau building

--- projet/main.py
+++ projet/main.py
@@ -1,23 +1,36 @@
 from data.code.utils import *
 
 autel1 = cl.Building('autel',(0,0),(2,2))
+autel1.lvl = 0
 orb1 = cl.Building('orb',(-1,1),(1,1))
 
 list_buildings = [autel1,orb1]
+placing = None
 
 while True:
     checkForQuit()
     DISPLAYSURF.fill(BGCOLOR)
     mouse_pos = pygame.mouse.get_pos ()
     to_highlight = None
-    
+
+    if placing is not None:
+        toPlace(placing,mouse_pos)
+
+    placing = None
+
     for building in list_buildings:
-        DISPLAYSURF.blit(b_imgs[building.type], relativeCoordsToPixels(building.pos))
-        if mouseOverBuilding(building,mouse_pos):
-            to_highlight = building
+        if building.lvl >= 0:
+            blitBuilding(building)
+            if mouseOverBuilding(building,mouse_pos):
+                to_highlight = building
+        else:
+            placing = building
     
-    if not to_highlight is None:
-        highlight(to_highlight.pos,to_highlight.size)
+    if to_highlight is not None:
+        if placing is None:
+            highlight(to_highlight.pos,to_highlight.size)
+        else:
+            showTranspaRed(to_highlight)
 
     pygame.display.update()
     FPSCLOCK.tick(FPS)
