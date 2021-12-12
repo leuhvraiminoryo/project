040a83cdbae476185425500cb6ffb512672d53bf
leuhvraiminoryo <50586611+leuhvraiminoryo@users.noreply.github.com>
2021-12-12 10:26:02 +0100

    fix highlight
    ye maintenant le highlight est fait après l'itération des bâtiments, comme ça il peut pas être overwrite par le bâtiment d'après

--- projet/main.py
+++ projet/main.py
@@ -9,11 +9,15 @@ while True:
     checkForQuit()
     DISPLAYSURF.fill(BGCOLOR)
     mouse_pos = pygame.mouse.get_pos ()
-
+    to_highlight = None
+    
     for building in list_buildings:
         DISPLAYSURF.blit(b_imgs[building.type], relativeCoordsToPixels(building.pos))
         if mouseOverBuilding(building,mouse_pos):
-            highlight(building.pos,building.size)
+            to_highlight = building
+    
+    if not to_highlight is None:
+        highlight(to_highlight.pos,to_highlight.size)
 
     pygame.display.update()
     FPSCLOCK.tick(FPS)
