992f8238ca0e2b227d9582927cec39c656dc77fb
leuhvraiminoryo <50586611+leuhvraiminoryo@users.noreply.github.com>
2021-12-22 18:16:05 +0100

    debut ajout nouvelle font (c'est buggé af)

--- projet/main.py
+++ projet/main.py
@@ -13,7 +13,8 @@ click = False
 while True:
     checkForQuit()
     DISPLAYSURF.fill(BGCOLOR)
-    text.show_text(str(e.ressources['soul_points']) + 'sp',2,10,1,9999,font,DISPLAYSURF)
+    text.show_text(str(e.ressources['soul_points']) + ' sp',2,10,1,9999,small_font,DISPLAYSURF)
+    text.show_text('Ceci est un petit test ... \\/:;,',160,50,5,99999,other_font,DISPLAYSURF)
     e.ressources['soul_points'] += 1
     mouse_pos = pygame.mouse.get_pos ()
     
