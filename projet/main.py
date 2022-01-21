3df490ce012c37fec525d50a277bf0eb563cad7d
leuhvraiminoryo <50586611+leuhvraiminoryo@users.noreply.github.com>
2022-01-21 17:59:19 +0100

    léger refactor verif keys
    ceci se passe désormais dans un fichier à part car voilà euh... faut rendre le main plus lisible

--- projet/main.py
+++ projet/main.py
@@ -1,21 +1,7 @@
 from data.code.utils import *
+from data.code.keys_verif import *
 
 placing = None
-left_click = False
-right_click = False
-
-
-
-pressed = {
-        "a" : False,
-        "r" : False,
-        "o" : False,
-        "d" : False,
-        "e" : False,
-        "w" : False,
-        "del" : False
-    }
-
 
 while True:
     checkForQuit(list_buildings,e.ressources)
@@ -30,59 +16,14 @@ while True:
     to_red = []
     to_menu = None
     posable = True
-    
-    for event in pygame.event.get():
-        if event.type == MOUSEBUTTONDOWN:
-            if event.button == 1:
-                left_click = True
-            if event.button == 3:
-                right_click = True
-        if event.type == MOUSEBUTTONUP:
-            if event.button == 1:
-                left_click = False
-            if event.button == 3:
-                right_click = False
-        pygame.event.post(event)
-            
 
-
-    for event in pygame.event.get(KEYUP):
-        if event.key == K_a:
-            pressed["a"] = False
-        if event.key == K_e:
-            pressed["e"] = False
-        if event.key == K_w:
-            pressed["w"] = False
-        if event.key == K_o:
-            pressed["o"] = False
-        if event.key == K_d:
-            pressed["d"] = False
-        if event.key == K_r:
-            pressed["r"] = False
-        if event.key == K_BACKSPACE:
-            pressed["del"] = False
-    
-    for event in pygame.event.get(KEYDOWN):
-        if event.key == K_a:
-            pressed["a"] = True
-        if event.key == K_e:
-            pressed["e"] = True
-        if event.key == K_w:
-            pressed["w"] = True
-        if event.key == K_o:
-            pressed["o"] = True
-        if event.key == K_d:
-            pressed["d"] = True
-        if event.key == K_r:
-            pressed["r"] = True
-        if event.key == K_BACKSPACE:
-            pressed["del"] = True
-    
+    pressed,left_click,right_click = verif_keys(pressed,left_click,right_click)
 
     if left_click:
         placing = None
     if placing is not None:
         toPlace(placing,mouse_pos)
+
     if True in pressed.values():
         
         if pressed["o"]:
